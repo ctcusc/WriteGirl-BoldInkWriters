d6dfdbf3a68093db49abdf9a6685cc1f873afac8
Ipek Goktan <ipekg@usc.edu>
2022-10-27 18:27:07 -0700

    frontend setup

--- frontend/WriteGirl/App.js
+++ frontend/WriteGirl/App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-native-paper'
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-export default function App() {
+import {ExamplePage} from "./src/pages"
+
+const Stack = createStackNavigator()
+
+export default function App({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Provider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="ExamplePage"
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="ExamplePage" component={ExamplePage} />
+            {/* add your screens here! */}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+      <Text>Hi Friends! Edit this page to display your new page from your ticket!</Text>
     </View>
   );
 }
---
+++ frontend/WriteGirl/src/pages/ExamplePage.js
@@ -0,0 +1,7 @@
+import {Text, Button } from 'react-native';
+
+export default function ExamplePage() {
+  return (
+    <Text>This is a test page!</Text>
+  )
+}
