6d62d15cabb5706aa7dc5add0170ee9c93501023
thaveesi <thaveesi@usc.edu>
2022-11-10 14:35:09 -0800

    first draft

--- frontend/WriteGirl/App.js
+++ frontend/WriteGirl/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-native-paper'
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {ExamplePage} from "./src/pages" 
+import NavBar, {ExamplePage} from "./src/pages" 
 // TODO: add your page here!
 
 const Stack = createNativeStackNavigator()
@@ -14,7 +14,8 @@ export default function App({navigation}) {
       <Stack.Navigator screenOptions={{
             headerShown: false,
           }}>
-        <Stack.Screen name="example page" component={ExamplePage} />
+        {/* <Stack.Screen name="example page" component={ExamplePage} /> */}
+        <Stack.Screen name="NavBar page" component={NavBar} />
         {/* TODO: add your page here! */}
       </Stack.Navigator>
     </NavigationContainer>
--- frontend/WriteGirl/src/pages/index.js
+++ frontend/WriteGirl/src/pages/index.js
@@ -1,2 +1,23 @@
 export { default as ExamplePage } from './ExamplePage'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ExamplePage from './ExamplePage';
+import RandomPage1 from './RandomPage1';
+import RandomPage2 from './RandomPage2';
+import RandomPage3 from './RandomPage3';
 // TODO: add your page here!
+
+const Tab = createBottomTabNavigator();
+
+export default function NavBar() {
+    return (
+    <Tab.Navigator
+        
+    >
+        <Tab.Screen name="Home" component={ExamplePage} />
+        <Tab.Screen name="Resources" component={RandomPage1} />
+        <Tab.Screen name="writing" component={RandomPage2} />
+        <Tab.Screen name="others" component={RandomPage3} />
+    </Tab.Navigator>
+    )
+  }
+  
