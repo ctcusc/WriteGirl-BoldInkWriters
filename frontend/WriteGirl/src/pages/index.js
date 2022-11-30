d8fe93722b84a9e8568c0ef8e1b82d6420f83493
Ipek Goktan <ipekg@usc.edu>
2022-11-29 17:45:24 -0800

    Revert "Revert "fixing formatting""
    This reverts commit 29a79dd13ae4a1c91381b8bf1e69adfd72f95ba0.

--- frontend/WriteGirl/src/pages/index.js
+++ frontend/WriteGirl/src/pages/index.js
@@ -14,9 +14,7 @@ const Tab = createBottomTabNavigator();
 
 export default function NavBar() {
     return (
-    <Tab.Navigator
-        
-    >
+    <Tab.Navigator>
         <Tab.Screen name="Home" component={HomePage} />
         <Tab.Screen name="Resources" component={RandomPage1} />
         <Tab.Screen name="Writing Experiments" component={WritingExperiments} />
