7b50a5aa87549be15d8ee572410eb354a9ea6106
ChelyFernandezUSC <cmfernan@usc.edu>
2023-01-31 12:01:56 -0800

    see exhibits & complete button

--- frontend/WriteGirl/src/pages/DoorActivity/DoorOpened.js
+++ frontend/WriteGirl/src/pages/DoorActivity/DoorOpened.js
@@ -5,11 +5,20 @@ export default function DoorOpened() {
 
     return(
         <SafeAreaView style={styles.container}>
-            <View style={styles.page} >
+            <View style={styles.page}>
                 <View style = {styles.openDiv}>
-                    <TouchableOpacity style={styles.openDoor}>
-                        <Text style = {styles.openText}>Door Opened!</Text> 
+                    <View style={styles.openDoor}>
+                        <Text style = {styles.openText}>Door Opened!</Text>
+                        <Text style = {styles.prompt}>Write a story about the door opening!</Text>
+                        <TouchableOpacity style={styles.interactive}>
+                            <Text style={styles.intText}>see exhibits</Text>
+                        </TouchableOpacity>
+                    </View>
+                    {/* Complete Button */}
+                    <TouchableOpacity style={styles.compButton}>
+                        <Text style={styles.compText}>Complete</Text>
                     </TouchableOpacity>
+
                 </View>   
             </View>
         </SafeAreaView>
