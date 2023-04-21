6a6a1f3138296c8ba5792388bb12ca71a20b324d
ChelyFernandezUSC <cmfernan@usc.edu>
2023-04-20 21:50:43 -0700

    door images

--- frontend/WriteGirl/src/pages/DoorActivity/DoorActivity.js
+++ frontend/WriteGirl/src/pages/DoorActivity/DoorActivity.js
@@ -3,9 +3,8 @@ import { useEffect, useState } from 'react';
 import {View, SafeAreaView, FlatList, Image, TouchableOpacity, Text} from 'react-native';
 import navigation from '@react-navigation/native';
 import { Alert, Collapse, NativeBaseProvider } from 'native-base';
-// import { NavigationContainer } from '@react-navigation/native';
-const imgArr = new Array(9).fill("https://placeimg.com/635/360/tech");
-const doorImage = require('../../../assets/door.png');
+const imgArr = new Array(9).fill().map((_, index) => `../../../assets/door${index}.png`);
+
 
 export default function DoorActivity({navigation}) {
     const [show, setShow] = useState(false);
@@ -25,15 +24,27 @@ export default function DoorActivity({navigation}) {
                         <Text style={styles.backText}>←</Text>
                     </TouchableOpacity>
                     <Text style={styles.chooseText}>Choose a Door!</Text>
-                    
 
                     <FlatList
-                        data={imgArr}
-                        renderItem={renderItem}
-                        ItemSeparatorComponent={() => <View/>}
-                        numColumns={3}
-                        key={3}
+                    data={imgArr}
+                    renderItem={({ item, index }) => {
+                        const doorImage = require(`../../../assets/door${index}.png`);
+
+                        return (
+                        <TouchableOpacity onPress={() => navigation.navigate("Door Opened")}
+                        style={styles.doorIcon} >
+                        <Image
+                            source={{ uri: doorImage }}
+                            style={{ width: "100%", height: 120 }}
+                        />
+                        </TouchableOpacity>
+                        );
+                    }}
+                    ItemSeparatorComponent={() => <View />}
+                    numColumns={3}
+                    key={3}
                     />
+
                     <NativeBaseProvider>
                         {show &&
                     <View>
@@ -54,18 +65,5 @@ export default function DoorActivity({navigation}) {
             </View>
         </SafeAreaView>
     )
-}
-
-function renderItem({navigation}) {
-
-
-    return (<TouchableOpacity  style={styles.doorIcon} onPress={() => {
-        navigation.navigate('Door Opened')
-        console.log("open door")
-        }}>
-            <Image source={doorImage} style={styles.doorImage}/>
-        </TouchableOpacity>);
-
-
 }
 
